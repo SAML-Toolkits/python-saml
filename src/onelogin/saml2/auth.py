f4658a0581bf30adf00c20190075b8e48543cd00
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2016-01-08 07:19:08 +0100

    [Add] Allow custom filename for "settings.json"
    This commit allows `OneLogin_Saml2_Settings` and `OneLogin_Saml2_Auth`
    to read in settings from a specified file instead of the default
    "settings.json", making it easier to change between configurations.

--- src/onelogin/saml2/auth.py
+++ src/onelogin/saml2/auth.py
@@ -49,7 +49,6 @@ class OneLogin_Saml2_Auth(object):
         :type custom_base_path: string
         """
         self.__request_data = request_data
-        self.__settings = OneLogin_Saml2_Settings(old_settings, custom_base_path)
         self.__attributes = []
         self.__nameid = None
         self.__nameid_format = None
@@ -64,6 +63,11 @@ class OneLogin_Saml2_Auth(object):
         self.__last_assertion_not_on_or_after = None
         self.__last_request = None
         self.__last_response = None
+
+        if isinstance(old_settings, OneLogin_Saml2_Settings):
+            self.__settings = old_settings
+        else:
+            self.__settings = OneLogin_Saml2_Settings(old_settings, custom_base_path)
 
     def get_settings(self):
         """
