cebc37b67ffa440b4e0e98f5852045b7fd1b6ca3
Sixto Martin <pitbulk@gmail.com>
2017-04-16 18:07:24 +0200

    Add Pyramid demo example

---
+++ demo_pyramid/setup.py
@@ -0,0 +1,45 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_jinja2',
+    'pyramid_debugtoolbar',
+    'waitress',
+    'xmlsec',
+    'isodate',
+    'python-saml',
+]
+
+setup(
+    name='demo_pyramid',
+    version='0.0',
+    description='demo_pyramid',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+    author='',
+    author_email='',
+    url='',
+    keywords='web pyramid pylons',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    entry_points={
+        'paste.app_factory': [
+            'main = demo_pyramid:main',
+        ],
+    },
+)
