7ab94910ecc359e545417abecb1d10c52e8ee040
Sixto Martin <pitbulk@gmail.com>
2018-04-05 17:03:44 +0200

    Update defusedxml dependency.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=[
         'dm.xmlsec.binding==1.3.3',
         'isodate>=0.5.0',
-        'defusedxml==0.4.1',
+        'defusedxml>=0.4.1',
     ],
     extras_require={
         'test': (
