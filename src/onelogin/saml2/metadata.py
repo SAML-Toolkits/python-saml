2b283b5568e67d8139f46ad8e7426d4f37172d86
Sixto Martin <pitbulk@gmail.com>
2018-04-23 18:26:28 +0200

    Update copyright and license reference

--- src/onelogin/saml2/metadata.py
+++ src/onelogin/saml2/metadata.py
@@ -2,8 +2,8 @@
 
 """ OneLogin_Saml2_Metadata class
 
-Copyright (c) 2014, OneLogin, Inc.
-All rights reserved.
+Copyright (c) 2010-2018 OneLogin, Inc.
+MIT License
 
 Metadata class of OneLogin's Python Toolkit.
 
--- tests/src/OneLogin/saml2_tests/error_test.py
+++ tests/src/OneLogin/saml2_tests/error_test.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2014, OneLogin, Inc.
-# All rights reserved.
+# Copyright (c) 2010-2018 OneLogin, Inc.
+# MIT License
 
 import unittest
 from teamcity import is_running_under_teamcity
