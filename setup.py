d3dbe3e9788c312c79d78d4292b0d2792605a3c4
Sixto Martin <pitbulk@gmail.com>
2018-08-26 13:00:17 +0200

    Update dm.xmlsec.binding dependency to 1.3.7

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     },
     test_suite='tests',
     install_requires=[
-        'dm.xmlsec.binding==1.3.3',
+        'dm.xmlsec.binding==1.3.7',
         'isodate>=0.5.0',
         'defusedxml>=0.4.1',
     ],
